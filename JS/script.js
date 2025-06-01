777795070182689ed749ea367d21063c7a10c0bd
Said bouaziz <alphawork404@gmail.com>
2025-06-01 11:25:17 +0000

    fourth commit

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+//###### js file ######\------->> TITLE = 16
+const aside_btn = document.querySelector(".aside-btn");
+aside_btn.addEventListener("click", () => {
+  document.body.classList.toggle("shrink");
+});
